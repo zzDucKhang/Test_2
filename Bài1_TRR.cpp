71514cebb3ce827909b6d9945f012c5a627c5058
Test_1 <dinhkhang787@gmail.com>
2025-04-09 20:44:42 +0700

    bài 1

--- Bài1_TRR.cpp
+++ Bài1_TRR.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
 
-    for (int a = 0; a <= 1; ++a) {
-        for (int b = 0; b <= 1; ++b) {
-            for (int c = 0; c <= 1; ++c) {
+    for (int a = 1; a >= 0; --a) {
+        for (int b = 1; b >= 0; --b) {
+            for (int c = 1; c >= 0; --c) {
+
                 int result = (a & b) | c; // (a và b ) hoac c 
                 cout << a << " " << b << " " << c << " |      " << result << endl;
             }
